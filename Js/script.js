80e46781b7c669521c65499deaeaeccc994b8b04
Wcrsantos <130843772+Wcrsantos@users.noreply.github.com>
2023-09-30 18:25:22 -0300

    Game Over - Final
    - implementação das regras que resultam  em fim de jogo.
    - Criação da animação das nuvens.

--- Js/script.js
+++ Js/script.js
@@ -13,4 +13,32 @@ function jump(){
     }, 500);
 }
 
-document.addEventListener("keydown", jump)
+document.addEventListener("keydown", jump);
+
+
+// Criação e configuração que verifica se o mario encostou no tubo (pipe) e para o jogo
+
+const pipe = document.querySelector('.pipe');
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(marioImg).bottom.replace('px', "");
+
+    
+    
+    if(pipePosition <= 125 && marioPosition < 80 && pipePosition > 0){
+
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        marioImg.style.animation = 'none';
+        marioImg.style.bottom = `${marioPosition}px`;
+
+        marioImg.src = "./images/game-over.png";
+        marioImg.style.width = "75px";
+        marioImg.style.marginLeft = "50px";
+
+        clearInterval(loop);
+    }
+},10)
